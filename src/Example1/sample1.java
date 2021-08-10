06916080c808928d7463a4a0afa14e43e4e9d72f
rushi <rushi@DESKTOP-S85RN5P>
2021-08-10 22:26:10 +0530

    changes by Rushi

--- src/Example1/sample1.java
+++ src/Example1/sample1.java
@@ -4,6 +4,8 @@ public class sample1 {
 
 	public static void main(String[] args) {
           System.out.println("hello I am Mangesh Made changes");
+          
+          System.out.println("hello guys i done some changes see it ");
 	}
 
 }
